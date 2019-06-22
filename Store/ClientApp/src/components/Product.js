d79235861ca67fe06f14a3d4663723780b5690eb
devinaharidas <devinaharidas@gmail.com>
2019-06-22 13:54:44 +1200

    third commit

--- Store/ClientApp/src/components/Product.js
+++ Store/ClientApp/src/components/Product.js
@@ -1,38 +1,63 @@
 ﻿import React, { Component } from 'react';
 import styled from 'styled-components';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import { Card,  Image } from 'semantic-ui-react'
  export class Product extends Component {
     state = {} 
      render() {
 
 
          const { img, title, id, price, inCart, product} = this.props;
-         return (<ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+         return (
+             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
 
-             <div className="card">
-                 <div className="img-container p-5"
-                     onClick={console.log("clicked on the image container")}>
+                 <div className="card">
+                     <div className="img-container p-5"
+                         onClick={console.log("clicked on the image container")}>
+                     </div>
+                     <Link to="/detailList">
+                         <img src={img}/></Link>
+                     <button className="cart-btn">Incart</button>
+                     <div className="card-footer d-flex justify-content-between">
+                         <p className="align-self-center mbo">{title}</p>
+
+                         <h5 className="text-blue font-italic mb-0">
+                             <span className="mr-1">${price}</span>
+
+                         </h5>
+                     </div>
+
+
+
                  </div>
-                 <Link to="/detailList">
-                     <img src={img} className="card-img-top" /></Link>
-                 <button className="cart-btn">Incart</button>
-                 <div className="card-footer d-flex justify-content-between">
-                     <p className="align-self-center mbo">{title}</p>
-                           
-                     
-                  h1    
-     
-                   
-                 </div>
-             </div>
 
 
-         </ProductWrapper>
+
+             </ProductWrapper>
+
+         
+       
+             
+
+
+        
+         )
+
+       
             
-        );
+       
     }
 }
 
 const ProductWrapper = styled.div`
+.card{
+border-color:blue;
+background: #F5F5F5;
+transition:all 1s linear;
+
+}
+
+
+`;
  
-`
--- Store/ClientApp/src/components/contextApi.js
+++ Store/ClientApp/src/components/contextApi.js
@@ -15,6 +15,27 @@ import { storeProducts, detailProduct } from './data.js'
 
          console.log("hello from cart");
      }
+
+     tester = () => {
+         console.log("data from state", this.state.productLists[0].inCart)
+         console.log("data ", storeProducts[0].inCart)
+         const tempProducts = [...this.state.productLists]
+         tempProducts[0].inCart = true;
+         this.setState(() => {
+
+             return { productLists: tempProducts }
+         },
+             () => {
+
+                 console.log("data from state", this.state.productLists[0].inCart)
+                 console.log("data ", storeProducts[0].inCart)
+
+             }
+           
+
+
+         )
+     }
     render() {
         return (
             <productContext.Provider value={{
@@ -24,6 +45,7 @@ import { storeProducts, detailProduct } from './data.js'
                 addToCart: this.addToCart
 
             }}>
+                <button onClick={this.tester}>Click me</button>
                 {this.props.children}
             </productContext.Provider>
 
